eaa3d5a101e7fb6980a6ff78ae21b5c1942c8531
Scong <sc4242@163.com>
2019-01-29 14:17:48 +0800

    随机密码生成器

--- Python_Script/random_pass.py
+++ Python_Script/random_pass.py
@@ -1,20 +1,22 @@
-# -*- coding utf-8 -*-
+# -*- coding: utf-8 -*-
 # By Scong
 # Date 2019-01-29
-# Version 1.0.1
+# Version 1.0.3
 
 
-# 随机密码生成器
+# 相关模块
 import random
 import string
 import getpass
 
-# 获得所有字符串
-all_char = string.ascii_letters + string.digits + string.punctuation
+# 获得所有的字符串
+all_char = string.ascii_letters + string.digits
 
-# 定义随机密码函数
+# 定义随机生成密码函数
 def get_pass(get_num):
     result = ""
+	
+	# 循环用户输入长度，最后输出随机密码
     for i in range(get_num):
         char = random.choice(all_char)
         result += char
@@ -22,16 +24,12 @@ def get_pass(get_num):
 
 
 if __name__ == '__main__':
-
-	#循环判断用户输入的是否为数字，如果不是则警告
-   while True:
-   # 异常处理
-       try:
-           get_num = int(input("请输入你要生成的密码长度："))
-           password = get_pass(get_num)
-           print("密码为：",password)
-           exit()
-       except ValueError:
-           print("\n\033[31;1m请输入正确的密码位数！\033[0m\n")
-
-
+    # 循环判断用户输入的信息，并捕获异常并处理
+    while True:
+        try:
+               get_num = int(input("Please Enter the length of the password you want to generate:"))
+               password = get_pass(get_num)
+               print("The password is : %s"% (password))
+               exit()
+        except ValueError:
+               print("\n\033[31;1m Please enter the correct password number!\033[0m\n")
